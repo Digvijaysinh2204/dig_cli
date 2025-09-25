cdaea871a23db1ae42e9b68ff34ef5fb6103061c
Digvijaysinh Chauhan <Digvijaysinh.chauhan@sapphiresolutions.net>
2025-09-25 19:12:59 +0530

    New version push

--- lib/src/version_helper.dart
+++ lib/src/version_helper.dart
@@ -2,4 +2,4 @@
 
 // The version is now hardcoded here.
 // Remember to change this manually when you release a new version.
-const String kDigCliVersion = '1.0.3';
+const String kDigCliVersion = '1.0.5';
